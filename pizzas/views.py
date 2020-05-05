afff2ea966c98a3a178f561f745ede44c6e396fe
Sijia <sijia_cai1@baylor.edu>
2020-05-05 02:25:40 -0500

    add more

--- pizzas/views.py
+++ pizzas/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render, redirect
 from .models import Pizza
 from .forms import CommentForm
+from django.contrib.auth.decorators import login_required
+from django.http import Http404
 
 # Create your views here.
 
@@ -9,21 +11,25 @@ def index(request):
     #The home page for pizzeria.
     return render(request, 'pizzas/index.html')
 
-
+@login_required
 def pizzas(request):
-    pizzas = Pizza.objects.all()
+    pizzas = Pizza.objects.filter(owner=request.user).all()
     context = {'pizzas':pizzas}
     return render(request, 'pizzas/pizzas.html', context)
 
-
+@login_required
 def pizza(request,pizza_id):
     pizza = Pizza.objects.get(id=pizza_id)
+    #make suer the pizza belongs to the current user
+    if pizza.owner != request.user:
+        raise Http404
+    # foreign key can be accessed using '_set'
     toppings = pizza.topping_set.all()
     comments = pizza.comment_set.all()
     context = {'pizza':pizza, 'toppings':toppings, 'comments':comments}
     return render(request, 'pizzas/pizza.html', context)
 
-
+@login_required
 def new_comment(request,pizza_id):
     pizza = Pizza.objects.get(id=pizza_id)
     if request.method != 'POST':
