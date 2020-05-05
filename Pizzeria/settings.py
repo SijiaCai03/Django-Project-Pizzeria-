28da6f2e2aee0c0ccf0cae138c3af5c6aa6143ba
Sijia <sijia_cai1@baylor.edu>
2020-05-05 04:47:29 -0500

    styling step

--- Pizzeria/settings.py
+++ Pizzeria/settings.py
@@ -33,6 +33,8 @@ ALLOWED_HOSTS = []
 INSTALLED_APPS = [
     'pizzas',
     'users',
+    #thrid party app
+    'bootstrap4',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
